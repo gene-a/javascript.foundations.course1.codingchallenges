018489da8f8c6d5a998f35e87d297bb5389aa4a1
gene-a <geneadina@gmail.com>
2021-05-29 13:02:12 +0900

    modified fundamentals 2 challenge 2

--- Javascript Fundamentals - 2/Challenge-2.js
+++ Javascript Fundamentals - 2/Challenge-2.js
@@ -8,8 +8,10 @@ const calcTip = function(bill) {
 const challenge2Case1 = function() {
     const bills = [125, 555, 44];
     const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+    const tips = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
     console.log(`Bills: ${bills}`);
     console.log(`Tips: ${tips}`);
+    console.log(`Totals: ${tips}`);
 }
 
 challenge2Case1();
