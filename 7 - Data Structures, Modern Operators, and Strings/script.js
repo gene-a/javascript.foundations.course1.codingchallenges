67c02f8b6d608b3749c4dcc633114ea112be5dc7
gene-a <geneadina@gmail.com>
2021-11-20 00:11:57 +0900

    added string manipulation exercises

---
+++ 7 - Data Structures, Modern Operators, and Strings/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const displaySanitizedText = function (inputValue) {
+  const values = inputValue.split('\n');
+  let valueIndex = 1;
+  for (const value of values) {
+    let [outputString, ...restOfString] = value.trim().split('_');
+    outputString = outputString.toLowerCase();
+    for (let i = 0; i < restOfString.length; i++) {
+      outputString +=
+        restOfString[i][0].toUpperCase() +
+        restOfString[i].slice(1).toLowerCase();
+    }
+    let outputPaddedSpace = outputString.padEnd(30, ' ');
+    let outputPaddedCheck = outputPaddedSpace.padEnd(
+      outputPaddedSpace.length + valueIndex++,
+      '✅'
+    );
+
+    console.log(outputPaddedCheck);
+  }
+};
+
+const onButtonClick = function () {
+  const button = document.querySelector('button');
+  button.addEventListener('click', function () {
+    displaySanitizedText(document.querySelector('textarea').value);
+  });
+};
+
+onButtonClick();
+
+// Test Cases
+// underscore_case
+// first_name
+// Some_Variable
+// calculate_AGE
+// delayed_departure
+// a_b
